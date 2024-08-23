de54201d0240b7e4a30e31f7b529a8632750e519
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2024-08-23 21:28:24 +0200

    remove console.log

--- server/src/middlewares/validationErrorHandler.ts
+++ server/src/middlewares/validationErrorHandler.ts
@@ -4,7 +4,6 @@ import { ValidationError } from "class-validator";
 export function validationErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
   if (Array.isArray(err) && err.every((e) => e instanceof ValidationError)) {
     const messages = err.map((error: ValidationError) => Object.values(error.constraints || {})).flat();
-    console.error("Validation Errors: daasdasdsa", messages);
     return res.status(400).json(messages);
   }
   next(err);
